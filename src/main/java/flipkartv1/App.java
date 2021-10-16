3b09eaebf52c2b340245090ed57a248f70579d35
svvskumar1128 <svvskumar@yahoo.com>
2021-10-16 01:31:05 -0700

    flipkart sale code

---
+++ src/main/java/flipkartv1/App.java
@@ -0,0 +1,13 @@
+package flipkartv1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello skumar!" );
+    }
+}
